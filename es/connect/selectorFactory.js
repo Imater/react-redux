7ad2806cd5eb176adeaffd450521ddd6b8647c2e
Nick Bespalov <nbespalov@me.com>
2017-09-25 18:27:40 +0500

    test

--- es/connect/selectorFactory.js
+++ es/connect/selectorFactory.js
@@ -1,5 +1,6 @@
 import shallowEqual from '../utils/shallowEqual';
 
+
 function _objectWithoutProperties(obj, keys) {
   var target = {};for (var i in obj) {
     if (keys.indexOf(i) >= 0) continue;if (!Object.prototype.hasOwnProperty.call(obj, i)) continue;target[i] = obj[i];
